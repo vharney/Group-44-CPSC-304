6519df959baf9c1ed1897dbd7f9fe500be3db5df
vieriharney <vieriharney@gmail.com>
2022-03-16 18:47:03 -0700

    Try first insert query

--- backend/index.php
+++ backend/index.php
@@ -21,6 +21,11 @@
     if (!empty($_POST)) {
         $content = json_decode(array_keys($_POST)[0], true);
         $response = array("success" => true, "message" => $content);
+        // echo $content["username"];
+        // echo $content["password"];
+        $query="insert into users values('".$content["username"]."', '".$content["password"]."', '".$content["phone"]."', '".$content["city"]."', '".$content["acode"]."', '".$content["fullName"]."')";
+        // echo $query;
+        mysqli_query($conn, $query);
         echo json_encode($response);
     }
 
