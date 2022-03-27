5fb3b465a71587f35d82de1260643d273f5538f9
vieriharney <vieriharney@gmail.com>
2022-03-27 16:33:19 -0700

    add profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ import UpdateFailed from './pages/UpdateFailed';
 import RegisterSuccess from './pages/RegisterSuccess';
 import RegisterFailed from './pages/RegisterFailed';
 import Groups from './pages/Groups';
+import Profile from './pages/Profile';
 
 function App() {
     return(
@@ -34,6 +35,7 @@ function App() {
                 <Route path="/registerSuccess" component={RegisterSuccess} />
                 <Route path="/registerFailed" component={RegisterFailed} />
                 <Route path="/:username/groups" component={Groups} />
+                <Route path="/:username/profile" component={Profile} />
             </Switch>
         </Router>
     );
