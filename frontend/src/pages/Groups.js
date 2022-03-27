7fee5ed52aef51e1ff8023fe246cc98a8808a4ca
vieriharney <vieriharney@gmail.com>
2022-03-27 03:05:32 -0700

    add join group button

--- frontend/src/pages/Groups.js
+++ frontend/src/pages/Groups.js
@@ -2,8 +2,9 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Box, Typography, Card, CardHeader, CardContent } from '@mui/material';
+import { Box, Typography, Card, Button, CardHeader, CardContent } from '@mui/material';
 import axios from 'axios';
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
 
 function AvailableGroups() {
     const { username } = useParams();
@@ -27,26 +28,55 @@ function AvailableGroups() {
         })
     }, []) 
 
+    const handleJoinGroup = (event) => {
+        axios.post("http://localhost:9000/api/groups/joinGroup.php", JSON.stringify({
+            username: {username}.username,
+            groupID: event.target.id
+        }))
+        .then((response) => {
+            if (response.data.message === "error") {
+                // Go back to login page if there is any error
+                console.log(response);
+            }
+            else {
+                // Else display home page accordingly
+                window.location.reload(false);
+            }
+        }, (error) => {
+            console.log(error);
+        })
+    }
+
     return (
         <div>
             <h1 style={{textAlign: "center"}}>
                 Available Groups to Join
             </h1>
-            {availableGroups.length > 0 ? availableGroups.map((yourGroup) => {
+            {availableGroups.length > 0 ? 
+            <div style={{marginBottom: "40px"}}>
+                {availableGroups.map((availableGroup) => {
                 // Iterate pages to create pages tabs in AppBar
                 return <Box sx={{ display: 'flex', flexWrap: 'wrap', '& > :not(style)': { m: "auto", mt: 2, width: 500, height: "fitContent"}}} >
                     <Card variant="outlined" sx={{ maxWidth: 500 }} style={{borderRadius: "20px", position: "relative"}} >
                         <CardHeader
-                            title={yourGroup[0].groupName}
+                            title={availableGroup[0].groupName}
                         />
                         <CardContent>
                             <Typography variant="body2">
-                                Member Number: {yourGroup[0].memberNum}
+                                Member Number: {availableGroup[0].memberNum}
                             </Typography>
                         </CardContent>
+                        <Button style={{position: "absolute", right: 10, bottom: 10}} onClick={handleJoinGroup} id={availableGroup[0].groupID} >
+                            <MeetingRoomIcon />
+                            Join Group
+                        </Button>
                     </Card>
                 </Box>
-            }) : <div style={{textAlign: "center" }}> No more available groups for you to join! Contact admin to add more group </div>}
+                })}
+            </div>
+            :
+            <div style={{textAlign: "center", marginBottom: "40px" }}> No more available groups for you to join! Contact admin to add more group </div>
+            }
         </div>
     );
 }
