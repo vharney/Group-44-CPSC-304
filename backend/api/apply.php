4bd1a924ec343f466c9920e23bf7cf8a3bdbbec0
Tylo Roberts <tyloroberts@dhcp-128-189-253-106.ubcsecure.wireless.ubc.ca>
2022-03-30 22:15:07 -0700

    not working version of apply.php

--- backend/api/apply.php
+++ backend/api/apply.php
@@ -1,22 +1,30 @@
 <?php
     include '../index.php'; // This include statement will allow us to use $conn from index.php which will be the connection to the MySQL DB
 
-    $username_comp = 'apple_official';
-    $username_emp = 'geek42';
-    $title = 'Graphic Designer';
-    $status = 'pending';
+    if (!empty($_POST)) {
+        $content = json_decode(array_keys($_POST)[0], true);
 
-    $query="INSERT INTO Applies(username_comp, username_emp, title, status) VALUES 
-            ((SELECT username from Jobs WHERE username='$username_comp'), '$username_emp', (SELECT title from Jobs WHERE title='$title'), '$status')";
+        $title = explode("*",$content["title"])[0];
+        $username_comp_str = explode("*",$content["title"])[1];
+        $username_emp = $content["username_emp"];
+        $status = 'pending';
 
+        if (!is_null($title) && !is_null($username_comp_str)) {
+            $query="INSERT INTO Applies(username_comp, username_emp, title, status) VALUES 
+            ((SELECT username from Jobs WHERE username='$username_comp_str' AND title='$title'), '$username_emp', '$title', '$status')";
   
-    echo $query;
-    // mysqli_query($conn, $query);
-    if ($conn->query($query) === TRUE){
-        echo "Record added successfully";
-    } else {
-        echo "Error adding record: " . $conn->error;
+            echo $query;
+            // mysqli_query($conn, $query);
+            if ($conn->query($query) === TRUE){
+                echo "Record added successfully";
+            } else {
+                echo "Error adding record: " . $conn->error;
+            }
+        }
+
     }
+
+
     // if (!empty($_POST)) {
     //     $content = json_decode(array_keys($_POST)[0], true);
     //     $response = array('success' => true, 'message' => $content);
