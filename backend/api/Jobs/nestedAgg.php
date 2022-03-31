2de37f08b180e5deff0ce31e842010e8f4f74f97
Mellisa Hadipranata <mellisahadipranata.preface@gmail.com>
2022-03-31 01:38:33 -0700

    add nested aggregration API for Jobs

---
+++ backend/api/Jobs/nestedAgg.php
@@ -0,0 +1,28 @@
+
+<?php
+    include '../../index.php'; // This include statement will allow us to use "$conn" from index.php which will be the connection to the MySQL DB
+
+    if (!empty($_POST)) {
+        $content = json_decode(array_keys($_POST)[0], true);
+
+        $query="SELECT title, ROUND(AVG(DISTINCT salary))
+            FROM Jobs 
+            GROUP BY title";
+
+        $result = mysqli_query($conn, $query);
+
+        if (mysqli_num_rows($result) > 0) { # If result is more than 0, user found return back the username in the response
+            $return = array();
+            
+            while($row = mysqli_fetch_array($result)) {
+                array_push($return, array($row));
+            }
+            $response = array("success" => true, "message" => $return);
+        }
+        else {
+            $response = array("success" => true, "message" => "error");
+        }
+
+        echo json_encode($response);
+    }
+?>
